553a4b15977e920a3f9615c08982a00d88308a4c
Alessandro DI BELLA <dib.alessandro@gmail.com>
2018-02-27 17:54:29 +0100

    added preference for sort order

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.android.popularmovies;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.android.popularmovies.util.MovieDBJsonUtils;
 import com.example.android.popularmovies.util.NetworkUtils;
@@ -16,12 +21,15 @@ import org.json.JSONException;
 import java.io.IOException;
 import java.net.URL;
 
-public class MainActivity extends AppCompatActivity implements PosterAdapter.PosterAdapterOnClickHandler {
+public class MainActivity extends AppCompatActivity implements
+        PosterAdapter.PosterAdapterOnClickHandler,
+        SharedPreferences.OnSharedPreferenceChangeListener {
 
     private final static String TAG = MainActivity.class.getSimpleName();
 
     private RecyclerView mRecyclerView;
     private PosterAdapter mPosterAdapter;
+    private static boolean PREFERENCES_UPDATED = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,11 +45,17 @@ public class MainActivity extends AppCompatActivity implements PosterAdapter.Pos
         mRecyclerView.setAdapter(mPosterAdapter);
 
         fetchMoviesData();
+
+        PreferenceManager.getDefaultSharedPreferences(this)
+        .registerOnSharedPreferenceChangeListener(this);
     }
 
     private void fetchMoviesData() {
-        //TODO sort order from settings
-        String sortOrder = "top_rated";
+        SharedPreferences pref = PreferenceManager
+                .getDefaultSharedPreferences(this);
+        String orderKey = this.getString(R.string.pref_order_key);
+        String defaultValue = this.getString(R.string.pref_order_popular);
+        String sortOrder = pref.getString(orderKey, defaultValue);
         new FetchMoviesTask().execute(sortOrder);
     }
 
@@ -51,6 +65,11 @@ public class MainActivity extends AppCompatActivity implements PosterAdapter.Pos
         Intent movieDetailIntent = new Intent(this, MovieDetailActivity.class);
         movieDetailIntent.putExtra(getString(R.string.parcel_movie_data), movie);
         startActivity(movieDetailIntent);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+        PREFERENCES_UPDATED = true;
     }
 
     private class FetchMoviesTask extends AsyncTask<String, Void, Movie[]>{
@@ -83,5 +102,39 @@ public class MainActivity extends AppCompatActivity implements PosterAdapter.Pos
                 mPosterAdapter.setData(movies);
             }
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.popularmovies, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (PREFERENCES_UPDATED) {
+            fetchMoviesData();
+            PREFERENCES_UPDATED = false;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .unregisterOnSharedPreferenceChangeListener(this);
     }
 }
--- app/src/main/java/com/example/android/popularmovies/MovieDetailActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MovieDetailActivity.java
@@ -1,8 +1,12 @@
 package com.example.android.popularmovies;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -38,5 +42,23 @@ public class MovieDetailActivity extends AppCompatActivity {
 
         ratingTV = findViewById(R.id.rating_tv);
         ratingTV.setText(String.valueOf(movie.getVoteAverage()));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
